581f2dcf80dbd26ceae641171fafeff10947c61c
Enoch Tamulonis <enochtamulonis@gmail.com>
2025-10-28 16:28:44 +0700

    button section updates ui when settings are changed in realtime

--- app/models/ui_library.rb
+++ app/models/ui_library.rb
@@ -11,5 +11,7 @@ class UiLibrary < ApplicationRecord
     def update_preview_elements
       broadcast_replace_to(self, target: "preview-element", partial: "ui_libraries/preview_element",
         locals: { ui_library: self })
+      broadcast_replace_to(self, target: "button-section", partial: "ui_libraries/button_section",
+        locals: { ui_library: self })
     end
 end
