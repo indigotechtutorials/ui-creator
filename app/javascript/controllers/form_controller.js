24174f19780c6920dfaec8bc70352d63bbb9ac08
Enoch Tamulonis <enochtamulonis@gmail.com>
2025-10-28 16:26:17 +0700

    stop form ui from redirecting and add realtime preview on right

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="form"
 export default class extends Controller {
   submit() {
-    this.element.submit()
+    this.element.requestSubmit()
   }
 }
--- app/models/ui_library.rb
+++ app/models/ui_library.rb
@@ -3,4 +3,13 @@ class UiLibrary < ApplicationRecord
   friendly_id :name, use: :slugged
   validates_presence_of :name
   validates_uniqueness_of :name
+
+  after_update_commit :update_preview_elements
+
+  private
+
+    def update_preview_elements
+      broadcast_replace_to(self, target: "preview-element", partial: "ui_libraries/preview_element",
+        locals: { ui_library: self })
+    end
 end
